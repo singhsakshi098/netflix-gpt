963cb054e0e22b16341caa762f8401c6f51edfa8
sakshi <sakshi0981456@gmail.com>
2025-10-02 19:33:40 +0530

    fixes the api fetching of top rated and popular movies

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -11,11 +11,11 @@ const SecondaryContainer = () => {
       <div className =" bg-black">
         <div className="  mt-0 md:-mt-60 pl-4 md:pl-12 relative z-20">
             <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-            <MovieList title={"Popular"} movies={movies.nowPopularMovies}/>
-            <MovieList title={"Top Rated"} movies={movies.nowTopRatedMovies}/>
+            <MovieList title={"Popular"} movies={movies.PopularMovies}/>
+            <MovieList title={"Top Rated"} movies={movies.TopRatedMovies}/>
             <MovieList title={"Upcoming Movies"} movies={movies.UpcomingMovies}/>
             <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
-            <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies}/>
+            
 
       {/*
       MovieList - Popular
